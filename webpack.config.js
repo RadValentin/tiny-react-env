1e8caff8c46381d2f5c30462d709af86169912ec
Valentin Radulescu <radulescu.valentin@gmail.com>
2017-12-05 21:27:33 +0200

    Add favicon to html output

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
+      template: path.join(__dirname, 'public', 'index.html'),
+      favicon: path.join(__dirname, 'public', 'favicon.ico')
     })
   ]
 };
