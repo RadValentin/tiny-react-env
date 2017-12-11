d177ef3ebc8d972148e2b311a46965ff9411f196
Valentin Radulescu <radulescu.valentin@gmail.com>
2017-12-11 12:10:27 +0200

    Add references to article

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tiny-react-env
+
+
+This project was built to accompany an article on Codementor.
+
+Click [here](https://goo.gl/d85bDh) to learn how to set up Webpack + React + Babel + Sass
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,14 @@ import './App.scss';
 
 class App extends Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return <div>
+      <h1>Hello World!</h1>
+      <p>This project was built to accompany an article on Codementor.</p>
+      <p>
+        Click <a href="https://goo.gl/d85bDh">here</a> to learn how to set up
+        Webpack + React + Babel + Sass
+      </p>
+    </div>
   }
 }
 
